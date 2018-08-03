831fd9b19c7ecf086a688b27d741695c8d08db59
FeiXie-Liam <xiefei0887@163.com>
2018-08-03 22:38:53 +0800

    [xiefei] feature : add getById api for order

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,11 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Order;
+import com.example.demo.exception.OrderNotFound;
 import com.example.demo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/orders")
@@ -17,4 +25,15 @@ public class OrderController {
     public ResponseEntity getAll() {
         return ResponseEntity.ok(orderService.getAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.get(id));
+    }
+
+    @ExceptionHandler(OrderNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private void orderNotFoundHandler() {
+    }
+
 }
--- src/main/java/com/example/demo/service/OrderService.java
+++ src/main/java/com/example/demo/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Order;
+import com.example.demo.exception.OrderNotFound;
 import com.example.demo.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,9 @@ public class OrderService {
 
     public List<Order> getAll() {
         return orderRepository.findAll();
+    }
+
+    public Order get(Long id){
+        return orderRepository.findById(id).orElseThrow(OrderNotFound::new);
     }
 }
